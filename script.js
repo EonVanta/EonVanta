718a202ea3916f5403a1125af7d8371f92cc0a6b
EonVanta <eonvanta@gmail.com>
2025-02-14 08:37:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
+    let navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+
+    if (navLinks.classList.contains('active')) {
+        navLinks.style.opacity = "0";
+        setTimeout(() => {
+            navLinks.style.opacity = "1";
+        }, 100);
+    }
+}
+
+function toggleMenu() {
     document.querySelector('.nav-links').classList.toggle('active');
 }
