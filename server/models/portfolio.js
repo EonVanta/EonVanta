daa4b21094224663e3fddede767ae73116a9404e
EonVanta <eonvanta@gmail.com>
2025-02-14 09:17:56 +0200

    Update portfolio.js

--- server/models/portfolio.js
+++ server/models/portfolio.js
@@ -1,1 +1,15 @@
-h
+// server/models/portfolio.js
+const mongoose = require('mongoose');
+
+const portfolioSchema = new mongoose.Schema({
+    userId: String,
+    stocks: [
+        {
+            symbol: String,
+            quantity: Number,
+            purchasePrice: Number
+        }
+    ]
+});
+
+module.exports = mongoose.model('Portfolio', portfolioSchema);
